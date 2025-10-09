68c5564ce56204b317ba52ee1632a6c00b76791f
rongkangw <138689948+rongkangw@users.noreply.github.com>
2025-10-09 22:30:52 +0800

    Fix Java CI check (line more than 120 chars)

--- src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
@@ -27,6 +27,8 @@ public class DeleteMemberCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteMemberCommand.MESSAGE_USAGE));
+        assertParseFailure(
+                parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteMemberCommand.MESSAGE_USAGE)
+        );
     }
 }
