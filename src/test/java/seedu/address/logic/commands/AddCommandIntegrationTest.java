df47f126c18130e38bbb00513b7dab72c3c71d84
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2025-10-09 22:24:28 +0800

    Fix add command usage details

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -42,7 +42,7 @@ public class AddCommandIntegrationTest {
     public void execute_duplicatePerson_throwsCommandException() {
         Member memberInList = model.getAddressBook().getPersonList().get(0);
         assertCommandFailure(new AddCommand(memberInList), model,
-                AddCommand.MESSAGE_DUPLICATE_PERSON);
+                AddCommand.MESSAGE_DUPLICATE_MEMBER);
     }
 
 }
