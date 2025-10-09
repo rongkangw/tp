ae1d05c28eabf5929b1eca7125a42126fea39bde
rongkangw <138689948+rongkangw@users.noreply.github.com>
2025-10-09 21:41:07 +0800

    Change usage and success messages

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -19,11 +19,11 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
+            + ": Deletes the member identified by the index number used in the current displayed list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_DELETE_MEMBER_SUCCESS = "Member: %1$s deleted successfully";
 
     private final Index targetIndex;
 
@@ -41,8 +41,8 @@ public class DeleteCommand extends Command {
         }
 
         Member memberToDelete = lastShownList.get(targetIndex.getZeroBased());
-        model.deletePerson(memberToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(memberToDelete)));
+        model.deleteMember(memberToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_MEMBER_SUCCESS, Messages.format(memberToDelete)));
     }
 
     @Override
