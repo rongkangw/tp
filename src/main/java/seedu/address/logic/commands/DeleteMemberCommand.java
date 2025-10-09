5bdd1e64b750e7f35134e484691c806014e337aa
rongkangw <138689948+rongkangw@users.noreply.github.com>
2025-10-09 22:26:04 +0800

    Update javadocs

--- src/main/java/seedu/address/logic/commands/DeleteMemberCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteMemberCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.Model;
 import seedu.address.model.member.Member;
 
 /**
- * Deletes a person identified using it's displayed index from the address book.
+ * Deletes a member identified using it's displayed index from the member list.
  */
 public class DeleteMemberCommand extends Command {
 
