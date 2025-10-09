52da24465d5faa5fb8b50ba0821be04c7690dfac
Jason <jason.nathan.chang@gmail.com>
2025-10-09 23:12:24 +0800

    Rename ListCommand to ListMember Comamnd

--- src/main/java/seedu/address/logic/commands/ListMemberCommand.java
+++ src/main/java/seedu/address/logic/commands/ListMemberCommand.java
@@ -8,11 +8,11 @@ import seedu.address.model.Model;
 /**
  * Lists all persons in the address book to the user.
  */
-public class ListCommand extends Command {
+public class ListMemberCommand extends Command {
 
-    public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_WORD = "listMembers";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all members";
 
 
     @Override
