272f1a351e8413c9314b968c33f64db735aea8c3
Win Paing Oo <winpaing000@gmail.com>
2025-10-09 23:07:39 +0800

    Add event storage interface

---
+++ src/main/java/seedu/address/storage/EventStorage.java
@@ -0,0 +1,29 @@
+package seedu.address.storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataLoadingException;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.event.Event;
+
+/**
+ * Represents a storage for {@link seedu.address.model.event.Event}
+ */
+public interface EventStorage {
+
+    /**
+     * Returns the file path of the data file.
+     * @return
+     */
+    Path getEventFilePath();
+
+    Optional<Event> readEvents() throws DataLoadingException;
+
+    Optional<ReadOnlyAddressBook> readEvents(Path filepath) throws DataLoadingException;
+
+    void saveEvents(ReadOnlyAddressBook events) throws IOException;
+
+    void saveEvents(ReadOnlyAddressBook events, Path filePath) throws IOException;
+}
