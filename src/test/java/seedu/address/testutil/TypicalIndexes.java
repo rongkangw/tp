f3594492508bf786650cf10ef38346dd5ec266ef
rongkangw <138689948+rongkangw@users.noreply.github.com>
2025-10-15 23:09:00 +0800

    Add tests for Delete Event command

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -9,4 +9,8 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_MEMBER = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_MEMBER = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_MEMBER = Index.fromOneBased(3);
+
+    public static final Index INDEX_FIRST_EVENT = Index.fromOneBased(1);
+    public static final Index INDEX_SECOND_EVENT = Index.fromOneBased(2);
+    public static final Index INDEX_THIRD_EVENT = Index.fromOneBased(3);
 }
