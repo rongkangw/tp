4cd5650e1e84f15c1ccf8f21d42eaac67c1d0557
hongxun03 <119728307+hongxun03@users.noreply.github.com>
2025-10-20 15:31:44 +0800

    Write up javadoc

--- src/main/java/seedu/club/model/ListState.java
+++ src/main/java/seedu/club/model/ListState.java
@@ -1,5 +1,22 @@
 package seedu.club.model;
 
+import seedu.club.logic.commands.general.ClearCommand;
+import seedu.club.logic.commands.general.ExitCommand;
+import seedu.club.logic.commands.general.HelpCommand;
+
+/**
+ * Represents the current list state of the ClubBook.
+ * This state determines which list (members or events) is displayed in the UI.
+ *
+ * <p>The state changes when commands targeting different entities are executed:
+ * <ul>
+ *   <li>{@code MEMBER} – when the user executes member-related commands.</li>
+ *   <li>{@code EVENT} – when the user executes event-related commands.</li>
+ * </ul>
+ *
+ * For neutral commands like {@link ClearCommand}, {@link ExitCommand} and {@link HelpCommand}, the existing
+ * state is preserved.
+ */
 public enum ListState {
     MEMBER,
     EVENT
