8dc4f6b82bc25da8ef7f9cbea1676e07a9c860d2
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2025-10-29 15:16:12 +0800

    Fix checkstyle long line

--- src/test/java/seedu/club/logic/parser/member/FindMemberCommandParserTest.java
+++ src/test/java/seedu/club/logic/parser/member/FindMemberCommandParserTest.java
@@ -17,7 +17,8 @@ public class FindMemberCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindMemberCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                FindMemberCommand.MESSAGE_USAGE));
     }
 
     @Test
