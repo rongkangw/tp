457b8970310d897ed066033a1c62a20468cc864c
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2025-11-03 22:02:15 +0800

    Fix testcase fails

--- src/test/java/seedu/club/testutil/TypicalClubBook.java
+++ src/test/java/seedu/club/testutil/TypicalClubBook.java
@@ -3,6 +3,7 @@ package seedu.club.testutil;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import seedu.club.model.ClubBook;
 import seedu.club.model.event.Event;
@@ -12,15 +13,33 @@ import seedu.club.model.member.Member;
  * A utility class containing a list of {@code Member} and {@code Event} objects to be used in tests.
  */
 public class TypicalClubBook {
+    public static final Member ALICE = new MemberBuilder().withName("Alice Pauline")
+            .withEmail("alice@example.com").withPhone("94351253").withMemberRoles("President")
+            .withEventRoles("Orientation", "facilitator", "gamemaster").build();
+    public static final Member BENSON = new MemberBuilder().withName("Benson Meier")
+            .withEmail("johnd@example.com").withEventRoles("Movie Night", "FoodIC", "OIC")
+            .withPhone("98765432").withMemberRoles("VP", "Operations").build();
+    public static final Member CARL = new MemberBuilder().withName("Carl Kurz").withPhone("95352563")
+            .withEmail("heinz@example.com").build();
+    public static final Member DANIEL = new MemberBuilder().withName("Daniel Meier").withPhone("87652533")
+            .withEmail("cornelia@example.com").withMemberRoles("Secretary")
+            .withEventRoles("Movie Night", "FoodIC", "OIC").build();
+    public static final Member ELLE = new MemberBuilder().withName("Elle Meyer").withPhone("94822244")
+            .withEmail("werner@example.com").withEventRoles("Movie Night", "FoodIC", "OIC").build();
+    public static final Member FIONA = new MemberBuilder().withName("Fiona Kunz").withPhone("94824271")
+            .withEmail("lydia@example.com").withEventRoles("Beach Day", "Gamemaster", "SafetyOfficer").build();
+    public static final Member GEORGE = new MemberBuilder().withName("George Best").withPhone("94824425")
+            .withEmail("anna@example.com").withEventRoles("Beach Day", "Gamemaster", "SafetyOfficer").build();
+
     public static final Event ORIENTATION = new EventBuilder().withName("Orientation")
             .withFrom("151025 1200").withTo("171025 1800").withDetail("For freshmen")
-            .withEventRoles("facilitator", "gamemaster").build();
+            .withEventRoles("facilitator", "gamemaster").withRoster(Set.of(ALICE)).build();
     public static final Event MOVIE_NIGHT = new EventBuilder().withName("Movie Night")
             .withFrom("201025 1800").withTo("201025 2000").withDetail("Showing The Shining")
-            .withEventRoles("FoodIC", "OIC").build();
+            .withEventRoles("FoodIC", "OIC").withRoster(Set.of(BENSON, DANIEL, ELLE)).build();
     public static final Event BEACH_DAY = new EventBuilder().withName("Beach Day")
             .withFrom("251025 0900").withTo("251025 2000").withDetail("At Sentosa")
-            .withEventRoles("Gamemaster", "SafetyOfficer").build();
+            .withEventRoles("Gamemaster", "SafetyOfficer").withRoster(Set.of(FIONA, GEORGE)).build();
 
     // Manually added events
     public static final Event MEETING = new EventBuilder().withName("Meeting")
@@ -29,24 +48,6 @@ public class TypicalClubBook {
     public static final Event WORKSHOP = new EventBuilder().withName("Workshop")
             .withFrom("151025 1000").withTo("151025 1300")
             .withEventRoles("participant", "facilitator").build();
-
-    public static final Member ALICE = new MemberBuilder().withName("Alice Pauline")
-            .withEmail("alice@example.com").withPhone("94351253")
-            .withMemberRoles("President").withEventRoles(ORIENTATION.getRoles()).build();
-    public static final Member BENSON = new MemberBuilder().withName("Benson Meier")
-            .withEmail("johnd@example.com").withEventRoles(MOVIE_NIGHT.getRoles()).withPhone("98765432")
-            .withMemberRoles("VP", "Operations").build();
-    public static final Member CARL = new MemberBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").build();
-    public static final Member DANIEL = new MemberBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withMemberRoles("Secretary")
-            .withEventRoles(MOVIE_NIGHT.getRoles()).build();
-    public static final Member ELLE = new MemberBuilder().withName("Elle Meyer").withPhone("94822244")
-            .withEmail("werner@example.com").withEventRoles(MOVIE_NIGHT.getRoles()).build();
-    public static final Member FIONA = new MemberBuilder().withName("Fiona Kunz").withPhone("94824271")
-            .withEmail("lydia@example.com").withEventRoles(BEACH_DAY.getRoles()).build();
-    public static final Member GEORGE = new MemberBuilder().withName("George Best").withPhone("94824425")
-            .withEmail("anna@example.com").withEventRoles(BEACH_DAY.getRoles()).build();
 
     public TypicalClubBook() {} //to prevent instantiation
 
